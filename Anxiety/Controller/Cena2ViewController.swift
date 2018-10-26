a7742478845694fa3c214c4346e2b2ac649f5abc
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 09:59:14 -0400

    Cena 2 Elemento de Acessibilidade Ativado

--- Anxiety/Controller/Cena2ViewController.swift
+++ Anxiety/Controller/Cena2ViewController.swift
@@ -17,6 +17,7 @@ class Cena2ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Cena2ImageView.isAccessibilityElement = true
     }
     
     
