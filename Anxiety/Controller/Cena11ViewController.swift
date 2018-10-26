1f3fff43c4565442bc2b2687fc317a30a66fbdef
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 09:37:32 -0400

    Outlet Cena 11 Image View

--- Anxiety/Controller/Cena11ViewController.swift
+++ Anxiety/Controller/Cena11ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class Cena11ViewController: UIViewController {
     
+    @IBOutlet weak var Cena11ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
