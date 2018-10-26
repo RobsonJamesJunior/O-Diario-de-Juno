ab591264d40b7be536d3f128171ab429002f3491
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 10:06:02 -0400

    Cena 15 Elemento de Acessibilidade Image View Ativado

--- Anxiety/Controller/Cena15ViewController.swift
+++ Anxiety/Controller/Cena15ViewController.swift
@@ -16,6 +16,7 @@ class Cena15ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Cena15ImageView.isAccessibilityElement = true
     }
     
     
