5092f1175399b10401d0db74415420c293399cf8
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 10:09:49 -0400

    Cena 19 Elemento de Acessibilidade Image View Ativado

--- Anxiety/Controller/Cena19ViewController.swift
+++ Anxiety/Controller/Cena19ViewController.swift
@@ -16,6 +16,7 @@ class Cena19ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Cena19ImageView.isAccessibilityElement = true
     }
     
     
