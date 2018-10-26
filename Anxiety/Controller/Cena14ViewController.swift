67919cb9de05029917b50f754e2fad0404aea486
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 10:05:29 -0400

    Cena 14 Elemento de Acessibilidade Image View Ativado

--- Anxiety/Controller/Cena14ViewController.swift
+++ Anxiety/Controller/Cena14ViewController.swift
@@ -15,6 +15,7 @@ class Cena14ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Cena14ImageView.isAccessibilityElement = true
     }
     
     
