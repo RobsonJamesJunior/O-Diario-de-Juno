224f012e4c9873fcca93b23fa3bedd0511efcdff
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 10:02:22 -0400

    Cena 8 Elemento de Acessibilidade Image View Ativado

--- Anxiety/Controller/Cena8ViewController.swift
+++ Anxiety/Controller/Cena8ViewController.swift
@@ -15,6 +15,7 @@ class Cena8ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Cena8ImageView.isAccessibilityElement = true
     }
     
     
