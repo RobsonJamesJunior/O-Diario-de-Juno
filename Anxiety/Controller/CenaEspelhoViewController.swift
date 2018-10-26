23be1aa175e3f6767920c76ee9da6521144586ee
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-26 09:49:36 -0400

    Outlet Cena Espelho ImageView

--- Anxiety/Controller/CenaEspelhoViewController.swift
+++ Anxiety/Controller/CenaEspelhoViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CenaEspelhoViewController: UIViewController {
     
+    @IBOutlet weak var CenaEspelhoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
