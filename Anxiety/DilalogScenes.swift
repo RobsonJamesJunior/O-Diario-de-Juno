53447ec291991ce8994a3ec625f2a2e43d733ed7
Lucídio Andrade Barbosa deSouza <lucidio.abs@gmail.com>
2018-10-26 18:02:15 -0400

    Dialogos das cenas 1/2 (primeira parte)

---
+++ Anxiety/DilalogScenes.swift
@@ -0,0 +1,50 @@
+//
+//  DilalogScenes.swift
+//  Anxiety
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 26/10/18.
+//  Copyright © 2018 Joel Menezes Hamon. All rights reserved.
+//
+
+import Foundation
+
+let cena1:[String] = ["Acordei cansado hj pela manha",]
+
+let cena1_1:[String] = ["Não dormir muito na noite passada, passei horas e horas pensando.",]
+
+let cena2:[String] = ["Vamos la!",]
+
+let cena4:[String] = ["Esta tudo bem!","Estou feliz!","Que legal!",]
+
+let cena5:[String] = ["Huummm, qual roupa eu vou?", "Essa não, essa não… essa aqui será? Melhor outra.",]
+
+let cena6:[String] = ["Amigos","Bom dia", "Viagens", "Hilário", "HAHAHA","Trabalho","Faculdade", "Tchau","Achou errado!","Show!",]
+
+let cena11:[String] = ["Ele ta olhando pra mim?","Por que ele estaria olhando pra mim?",]
+
+let cena14:[String] = ["Será que é um assaltante?","Ta chegando perto, o que eu faço??",]
+
+let cena15:[String] = ["Vou direto ou pego outro caminho?",]
+
+let cena16:[String] = ["aaaahhhh",]
+
+let cena20_1:[String] = ["TA ATRASADO!!!!! Vou descontar do seu salário.","Pega o avental.","Ta bom.",]
+
+let cena20_2:[String] = ["Bom dia, vamos trabalhar?","To indo",]
+
+let cena23:[String] = ["Que isso cara? Ninguém aqui pediu isso não.","Desculpe senhor, vou ja trocar.","cuidado cara vai jogar as coisa em mim. Ta me vendo não?","Presta atenção!","Quero falar com o gerente!","Eu vou resolver senhor, me desculpe.",]
+
+let cena24:[String] = ["Preciso de um tempo!",]
+
+let cena27:[String] = ["ei? Tudo bem? Respira!!!!","Olha pra mim… isso… se acalme.","Quer uma água???","Claro!","Melhor agora?","To sim, obrigado!",]
+
+let cena28:[String] = ["Não deveria mas preciso fumar pra me acalmar.",]
+
+let cena29:[String] = ["Tchau, até amanhã","Até, cuidado!",]
+
+
+
+
+
+
+
