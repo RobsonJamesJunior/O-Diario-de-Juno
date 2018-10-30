f1e81570de5c9e74796ca0a2b48737868f3a36d1
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-10-30 16:38:33 -0300

    Mudança do nome do app de anxiety para Juno, criação de um arquivo swift no model para dicas

---
+++ Anxiety/Controller/TipsScenes.swift
@@ -0,0 +1,15 @@
+//
+//  TipsScenes.swift
+//  Anxiety
+//
+//  Created by Robson James Junior on 30/10/18.
+//  Copyright © 2018 Joel Menezes Hamon. All rights reserved.
+//
+
+import Foundation
+
+let dicaCenaCafe:[String] = ["Dica: chás podem ajudar a se acalmar!",]
+
+let dicaCenaMetro:[String] = ["Músicas podem diminuir a ansiedade.",]
+
+let dicaCenaRespiracao:[String] = ["Tente controlar sua respiração",]
