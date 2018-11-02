ed5c5c495472d8f15dfdce3b4f51eb89e8a8f672
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-02 16:45:39 -0300

    ajeitando imagens erradas..

--- Anxiety/Controller/Cena1ViewController.swift
+++ Anxiety/Controller/Cena1ViewController.swift
@@ -17,7 +17,7 @@ class Cena1ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Cena1ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
-        let Cena1Gif = UIImage.gifImageWithName("Cena1") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena1
+        let Cena1Gif = UIImage.gifImageWithName("Cena_1") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena1
         Cena1ImageView.image = Cena1Gif // Adicionando a variável à tela de ImageView
     }
     
