34506ba9970c66082e7a6e7377daaebcb00466da
Lucídio Andrade Barbosa deSouza <lucidio.abs@gmail.com>
2018-11-02 15:22:04 -0400

    add dica na cena cafe (6)

--- Anxiety/Controller/Cena6ViewController.swift
+++ Anxiety/Controller/Cena6ViewController.swift
@@ -10,7 +10,27 @@ import UIKit
 
 class Cena6ViewController: UIViewController {
     
+    @IBOutlet weak var DicasLabel: UILabel!
     @IBOutlet weak var Cena6ImageView: UIImageView!
+    
+    
+    override func viewWillAppear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+        
+        DicasLabel.center.x += view.bounds.height
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //animeted label
+        UIView.animate(withDuration: 1.0, animations:{
+            
+            self.DicasLabel.center.x -= self.view.bounds.height
+            
+        })
+    }
+
+    
     
     
     override func viewDidLoad() {
