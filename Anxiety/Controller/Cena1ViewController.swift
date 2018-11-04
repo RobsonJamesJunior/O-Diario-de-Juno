6417543d9be6f55f528dcf916aa6e22d484d91af
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-04 08:46:42 -0400

    Programando vibração da cena de acordar

--- Anxiety/Controller/Cena1ViewController.swift
+++ Anxiety/Controller/Cena1ViewController.swift
@@ -13,17 +13,34 @@ class Cena1ViewController: UIViewController {
     
     @IBOutlet weak var Cena1ImageView: UIImageView!
     
+    var timer: Timer!
+    var controlVibration: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         Cena1ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena1Gif = UIImage.gifImageWithName("Cena_1") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena1
         Cena1ImageView.image = Cena1Gif // Adicionando a variável à tela de ImageView
-        
-//        for _ in 1...5 { // Repetição da quantidade de vibrações
-//                AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) // Comando de vibrar
-//                sleep(1) // delay do comando
-//            }
+      
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Cena1ViewController.update), userInfo: nil, repeats: true)
+
+    }
+    
+    
+    @objc func update() { // Função de atualização para opreações constantes
+        if controlVibration == false{ // Variável que faz vibrar apenas uma vez
+            for _ in 1...5 { // Repetição da quantidade de vibrações
+                AudioServicesPlaySystemSound(kSystemSoundID_Vibrate) // Comando de vibrar
+                sleep(1) // delay do comando
+                controlVibration = true
+            }
+        } else{
+            
         }
+    }
+    
+    
+    
 }
