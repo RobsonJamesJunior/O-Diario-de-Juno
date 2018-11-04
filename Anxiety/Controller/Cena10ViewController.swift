420f065be80a43532c1eca892822ae5015fa2fa1
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-04 11:15:23 -0400

    excluíndo cena Espelho e organizando cenas

--- Anxiety/Controller/Cena10ViewController.swift
+++ Anxiety/Controller/Cena10ViewController.swift
@@ -14,11 +14,10 @@ class Cena10ViewController: UIViewController {
     @IBOutlet weak var Cena10ImageView: UIImageView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //Cena10ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
-        //let Cena10Gif = UIImage.gifImageWithName("Cena_10") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena10
-        //Cena10ImageView.image = Cena10Gif // Adicionando a variável à tela de ImageView
+        super.viewDidLoad() // Do any additional setup after loading the view, typically from a nib.
+        Cena10ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
+        let Cena10Gif = UIImage.gifImageWithName("Cena_10") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena10
+        Cena10ImageView.image = Cena10Gif // Adicionando a variável à tela de ImageView
     }
     
     
