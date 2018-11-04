48ad3a8bab220c37a28da4158dc839eed11285fb
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-04 16:33:58 -0400

    som da cena 5

--- Anxiety/Controller/Cena5ViewController.swift
+++ Anxiety/Controller/Cena5ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import AVFoundation // Biblioteca para utilizar sons
 
 class Cena5ViewController: UIViewController {
     
     @IBOutlet weak var Cena5ImageView: UIImageView!
+    
+     var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,14 @@ class Cena5ViewController: UIViewController {
         Cena5ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena5Gif = UIImage.gifImageWithName("Cena_5") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena5
         Cena5ImageView.image = Cena5Gif // Adicionando a variável à tela de ImageView
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "agua_pia", ofType: "mp3")!)) // colocando a música através do diretório
+            audioPlayer.prepareToPlay() // preparando o áudio
+        } catch {
+            print(error) // erro de áudio
+        }
+        
+        audioPlayer.play() // dá o play no áudio
     }
     
     
