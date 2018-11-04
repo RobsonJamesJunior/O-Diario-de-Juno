d1e29580c5995bfabd5402399c598542510f10e0
Joel Hamon <joelmhamon@gmail.com>
2018-11-04 17:47:55 -0400

    botões da cena 2

--- Anxiety/Controller/Cena2ViewController.swift
+++ Anxiety/Controller/Cena2ViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 class Cena2ViewController: UIViewController {
     
+    //esse é o colisor que sera usado para deletar as palavras
     
     @IBOutlet weak var Cena2ImageView: UIImageView!
+    
+    @IBOutlet weak var Palavra1: UILabel!
+    
+    @IBOutlet weak var Palavra2: UILabel!
+    
+    @IBOutlet weak var Palavra3: UILabel!
+    
+    @IBOutlet weak var Palavra4: UILabel!
+    
+    @IBOutlet weak var Palavra5: UILabel!
+    
+    @IBOutlet weak var Palavra6: UILabel!
+    
+    @IBOutlet weak var Palavra7: UILabel!
+    
+    @IBAction func StartGame(_ sender: Any) {
+        
+        
+    }
+    
+    func fazerBotoesDiminuir(){
+        
+    }
     
     
     override func viewDidLoad() {
@@ -20,6 +44,8 @@ class Cena2ViewController: UIViewController {
         Cena2ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena2Gif = UIImage.gifImageWithName("Cena_2") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena2
         Cena2ImageView.image = Cena2Gif // Adicionando a variável à tela de ImageView
+        
+        
     }
     
     
