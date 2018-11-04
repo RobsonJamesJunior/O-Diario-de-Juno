4db222b1eababa53016d5297e860b08bf198e13c
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-04 16:27:56 -0400

    Som da chave, interação Cena 8

--- Anxiety/Controller/Cena8ViewController.swift
+++ Anxiety/Controller/Cena8ViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import AVFoundation // Biblioteca para utilizar sons
 
 class Cena8ViewController: UIViewController {
     
     @IBOutlet weak var Cena8ImageView: UIImageView!
+    
+    
+     var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +22,19 @@ class Cena8ViewController: UIViewController {
         Cena8ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena8Gif = UIImage.gifImageWithName("Cena_8") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena8
         Cena8ImageView.image = Cena8Gif // Adicionando a variável à tela de ImageView
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "chave", ofType: "wav")!)) // colocando a música através do diretório
+            audioPlayer.prepareToPlay() // preparando o áudio
+        } catch {
+            print(error) // erro de áudio
+        }
+        
     }
     
+    
+    
+    @IBAction func keySegueScene(_ sender: Any) {
+        audioPlayer.play()
+    }  
     
 }
