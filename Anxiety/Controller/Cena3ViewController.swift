e0fa4c6153c8c2b12659c2ba4bdd0da0c9ca751f
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-04 20:14:56 -0400

    cena3 animação balão

--- Anxiety/Controller/Cena3ViewController.swift
+++ Anxiety/Controller/Cena3ViewController.swift
@@ -11,7 +11,8 @@ import AVFoundation
 
 class Cena3ViewController: UIViewController {
   
-    var timer = Timer()
+    var timer: Timer!
+    var IsOk: Int = 0
     
     @IBOutlet weak var Cena3ImageView: UIImageView!
     
@@ -22,6 +23,64 @@ class Cena3ViewController: UIViewController {
         Cena3ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena3Gif = UIImage.gifImageWithName("Cena_3") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena3
         Cena3ImageView.image = Cena3Gif // Adicionando a variável à tela de ImageView
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Cena3ViewController.update), userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    @objc func update() { // Função de atualização para opreações constantes
+        if IsOk < cena2.count { // Variável que faz vibrar apenas uma vez
+            showOutgoingMessage(text: cena2[IsOk])
+            sleep(1)
+            IsOk += 1
+        } else{
+            
+        }
+    }
+    
+    func showOutgoingMessage(text: String) {
+        let label =  UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .black
+        label.text = ""
+        
+        let constraintRect = CGSize(width: 0.66 * view.frame.width,
+                                    height: .greatestFiniteMagnitude)
+        let boundingBox = text.boundingRect(with: constraintRect,
+                                            options: .usesLineFragmentOrigin,
+                                            attributes: [.font: label.font],
+                                            context: nil)
+        label.frame.size = CGSize(width: ceil(boundingBox.width),
+                                  height: ceil(boundingBox.height))
+        
+        let bubbleImageSize = CGSize(width: label.frame.width + 28,
+                                     height: label.frame.height + 20)
+        
+        let outgoingMessageView = UIImageView(frame:
+            CGRect(x: view.frame.width - bubbleImageSize.width - 230, // posição x
+                y: view.frame.height - bubbleImageSize.height - 550, //posição y
+                width: bubbleImageSize.width,
+                height: bubbleImageSize.height))
+        
+        let bubbleImage = UIImage(named: "cidio")?
+            .resizableImage(withCapInsets: UIEdgeInsets(top: 17, left: 21, bottom: 17, right: 21),
+                            resizingMode: .tile)
+            .withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        
+        outgoingMessageView.image = bubbleImage
+        
+        view.addSubview(outgoingMessageView)
+        
+        label.center = outgoingMessageView.center
+        
+        let labelDialog = (Bundle.main.infoDictionary?["CFBoundleName"] as? String) ?? text
+        
+        for letra in labelDialog {
+            label.text! += ("\(letra)")
+            RunLoop.current.run(until: Date()+0.10)
+            view.addSubview(label)
+        }
     }
     
     
