1a778aec7cb0509868a632334143831573e8d9fa
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-11-05 02:29:37 -0400

    segues 2,3,4 automático segue

--- Anxiety/Controller/Cena4ViewController.swift
+++ Anxiety/Controller/Cena4ViewController.swift
@@ -12,7 +12,7 @@ class Cena4ViewController: UIViewController {
     
     
     @IBOutlet weak var Cena4ImageView: UIImageView!
-    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +20,13 @@ class Cena4ViewController: UIViewController {
         Cena4ImageView.isAccessibilityElement = true// Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena4Gif = UIImage.gifImageWithName("Cena_4") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena4
         Cena4ImageView.image = Cena4Gif // Adicionando a variável à tela de ImageView
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Cena4ViewController.update), userInfo: nil, repeats: true)
+        
     }
- 
+    
+    
+    @objc func update() { // Função de atualização para opreações constantes
+        performSegue(withIdentifier: "Segue4", sender: nil)
+    }
     
 }
