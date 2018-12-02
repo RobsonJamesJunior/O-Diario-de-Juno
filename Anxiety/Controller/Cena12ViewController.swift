c3fa08ae596d774cf20dd83aedc299b5d72b4ae6
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-12-02 15:54:54 -0400

    tirando jogo cena 12 para features

--- Anxiety/Controller/Cena12ViewController.swift
+++ Anxiety/Controller/Cena12ViewController.swift
@@ -9,72 +9,9 @@
 import UIKit
 
 class Cena12ViewController: UIViewController {
-    
-    var currentValue: Int = 0
-    var targetValue: Int = 0
-    var contadorDerounds: Int = 1
    
     @IBOutlet weak var pressToContinue: UIButton!
-    func startNewRound(){
-        if contadorDerounds > 3{
-            pressToContinue.isHidden = false
-        }
-        //contadorDerounds+=1
-       // round.text = String(contadorDerounds)
-        contadorDerounds+=1
-        
-        targetValue = 1 + Int(arc4random_uniform(100))
-        currentValue = 50
-        slider.value = Float(currentValue)
-    }
-    
-    func upDateLabel(){
-        targetLabel.text = String(targetValue)
-    }
-    
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var targetLabel: UILabel!
-    
-    @IBAction func showAlert(_ sender: Any) {
-        let diference:Int = abs(currentValue - targetValue)
-       // let point: Int = 100 - diference
-       // score += point
-        
-        
-        var title: String
-        
-        if diference == 0{
-            title = "Perfeito!"
-            //score += 100
-        }else if diference < 5{
-            title = "Quase acertou!"
-           // score += 50
-        }else{
-            title = "Passou longe!"
-        }
-        
-        let message: String = "\n\(title)" + "\nO valor que você colocou foi: \(currentValue) de \(targetValue)"
-        
-        let alert = UIAlertController(title: "Pontuação", message: message, preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-        
-       // scoreValue.text = String(score)
-        startNewRound()
-        upDateLabel()
-    }
-    
-    @IBAction func slideMoved(_ slider: UISlider) {
-        
-        currentValue = lroundf(slider.value)//arredonda o valor float
-        //print("O valor do slider é \(currentValue)")
-        
-        
-    }
+   
     @IBOutlet weak var Cena12ImageView: UIImageView!
     
     override func viewDidLoad() {
@@ -83,8 +20,7 @@ class Cena12ViewController: UIViewController {
         Cena12ImageView.isAccessibilityElement = true
         let Cena12Gif = UIImage.gifImageWithName("Cena_12") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena6
         Cena12ImageView.image = Cena12Gif // Adicionando a variável à tela de ImageView
-        startNewRound()
-        upDateLabel()
+
     }
     override var prefersStatusBarHidden: Bool{
         return true
