ceb586bb5507f6ddb93e785a7bb39f665e047ab4
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-12-03 22:32:06 -0400

    dinamica cena 19_1 swipe

--- Anxiety/Controller/Cena19_1ViewController.swift
+++ Anxiety/Controller/Cena19_1ViewController.swift
@@ -15,10 +15,29 @@ class Cena19_1ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        downSwipe.direction = .down
+        
+        view.addGestureRecognizer(downSwipe)
+        
         Cena19_1ImageView.isAccessibilityElement = true
         let Cena19_1Gif = UIImage.gifImageWithName("Cena_19_1") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena
         Cena19_1ImageView.image = Cena19_1Gif // Adicionando a variável à tela de ImageView
     }
+    
+    @objc func handleSwipe(sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            switch sender.direction {
+            case .down:
+                performSegue(withIdentifier: "segue19_2_1", sender: nil)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
     override var prefersStatusBarHidden: Bool{
         return true
     }
