b4058c9d7dc9b32e931af28642b9ee29d8f115a2
Robson James dos Reis Silva Júnior <james.robson.junior@gmail.com>
2018-12-04 15:29:26 -0400

    reprogramando dinâmica cena 8 chave e porta

--- Anxiety/Controller/Cena8ViewController.swift
+++ Anxiety/Controller/Cena8ViewController.swift
@@ -13,44 +13,10 @@ class Cena8ViewController: UIViewController {
     
     @IBOutlet weak var Cena8ImageView: UIImageView!
     // var audioPlayer = AVAudioPlayer()
+   
+    @IBOutlet weak var key: UIImageView!
     
-    @IBOutlet weak var keyImageView: UIImageView!
-    @IBOutlet weak var fechaduraImageView: UIImageView!
-    
-    var keyViewOrigin: CGPoint!
-    
-    func addPanGesture(view: UIView){
-        let pan = UIPanGestureRecognizer(target: self, action: #selector (Cena8ViewController.handlePan(sender:)))
-        view.addGestureRecognizer(pan)
-    }
-    
-    @objc func handlePan(sender: UIPanGestureRecognizer){
-        let keyView = sender.view!
-        let translation = sender.translation(in: Cena8ImageView)
-        switch sender.state {
-        case .began, .changed:
-            //move
-            keyView.center = CGPoint(x: keyView.center.x + translation.x , y: keyView.center.y + translation.y)
-            sender.setTranslation(CGPoint.zero, in: Cena8ImageView)
-        case .ended:
-            //delete
-            if keyView.frame.intersects(fechaduraImageView.frame){
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.keyImageView.alpha = 0.0
-                })
-               // audioPlayer.play()
-                performSegue(withIdentifier: "next", sender: self)
-            } else {
-                //return
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.keyImageView.frame.origin = self.keyViewOrigin
-                })
-            }
-        default:
-            break
-        }
-    }
-    
+    @IBOutlet weak var fech: UIImageView!
     
     
     override func viewDidLoad() {
@@ -61,6 +27,10 @@ class Cena8ViewController: UIViewController {
         Cena8ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena8Gif = UIImage.gifImageWithName("Cena_8") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena8
         Cena8ImageView.image = Cena8Gif // Adicionando a variável à tela de ImageView
+        
+        let gifChave = UIImage.gifImageWithName("key") // variável com gif do asset da chave
+        key.image = gifChave // setando o asset gif no ImageView
+        
 //        do {
 //            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "chave", ofType: "wav")!)) // colocando a música através do diretório
 //            audioPlayer.prepareToPlay() // preparando o áudio
@@ -68,17 +38,9 @@ class Cena8ViewController: UIViewController {
 //            print(error) // erro de áudio
 //        }
         
-        addPanGesture(view: keyImageView)
-        keyViewOrigin = keyImageView.frame.origin
-        Cena8ImageView.bringSubviewToFront(keyImageView)
-        
     }
     
-    
-    
-//    @IBAction func keySegueScene(_ sender: Any) {
-//        audioPlayer.play()
-//    }
+  
     
     override var prefersStatusBarHidden: Bool{
         return true
