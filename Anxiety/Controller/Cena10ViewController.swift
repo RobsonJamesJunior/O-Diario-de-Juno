b5abdf519ada3133ccbc997db385ddffcd83b9c8
Lucídio Souza <lucidio.abs@gmail.com>
2019-04-26 08:48:42 -0400

    sounds cena 10

--- Anxiety/Controller/Cena10ViewController.swift
+++ Anxiety/Controller/Cena10ViewController.swift
@@ -14,8 +14,10 @@ class Cena10ViewController: UIViewController {
     
     @IBOutlet weak var Cena10ImageView: UIImageView!
     var labelEnd: Bool = false
-    var audioPlayer = AVAudioPlayer()
+    //var audioPlayer = AVAudioPlayer()
     var initialView: Bool = false
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad() // Do any additional setup after loading the view, typically from a nib.
@@ -23,7 +25,14 @@ class Cena10ViewController: UIViewController {
         Cena10ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena10Gif = UIImage.gifImageWithName("Cena_10") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena10
         Cena10ImageView.image = Cena10Gif // Adicionando a variável à tela de ImageView
+        MusicHelper.sharedHelper.audioPlayer?.volume = 0.3
+        Transito.sharedHelper.playBackgroundMusic()
+        Transito.sharedHelper.audioPlayer?.volume = 1.5
+        
+        
     }
+    
+    
     
     
     @IBAction func StopSounTraffic(_ sender: Any) {
