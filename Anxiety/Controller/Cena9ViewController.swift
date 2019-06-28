b060e3e82feba45306cf50390705895962970bb1
Robson James <james.robson.junior@gmail.com>
2019-06-28 12:35:21 -0300

    elevador Dinamica

--- Anxiety/Controller/Cena9ViewController.swift
+++ Anxiety/Controller/Cena9ViewController.swift
@@ -15,7 +15,21 @@ class Cena9ViewController: UIViewController {
     
     
     
+    @IBOutlet weak var backGroundPopUp: UIImageView!
+    
+    @IBOutlet weak var painelView: UIImageView!
+    
+    @IBOutlet weak var nexButtonElevator: UIButton!
+    
     @IBOutlet weak var Cena9ImageView: UIImageView!
+    
+   
+    @IBAction func activeElevatorPainel(_ sender: Any) {
+        backGroundPopUp.isHidden = false
+        painelView.isHidden = false
+        nexButtonElevator.isHidden = false
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +38,12 @@ class Cena9ViewController: UIViewController {
         Cena9ImageView.isAccessibilityElement = true // Comando que transforma a ImageView em um objeto visível pelo crossover
         let Cena9Gif = UIImage.gifImageWithName("Cena_9") // Cria uma variável com a imagem Gif através da extensão da biblioteca ImageView que será utilizada na ImageView da Cena9
         Cena9ImageView.image = Cena9Gif // Adicionando a variável à tela de ImageView
+        
+        backGroundPopUp.isHidden = true
+        painelView.isHidden = true
+        nexButtonElevator.isHidden = true
+        
+        
     }
     
     override var prefersStatusBarHidden: Bool{
